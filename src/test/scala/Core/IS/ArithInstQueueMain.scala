497d357daab71a00639f83b21148b73ec7d8540a
huxuan0307 <532391012@qq.com>
2022-03-03 23:37:41 +0800

    Refactor: RS->IS

--- src/test/scala/Core/IS/ArithInstQueueMain.scala
+++ src/test/scala/Core/IS/ArithInstQueueMain.scala
@@ -1,12 +1,12 @@
-package Core.RS
+package Core.IS
 
 import chisel3.stage.{ChiselGeneratorAnnotation, ChiselStage}
 
-object DepRegEntryMain extends App {
+object ArithInstQueueMain extends App {
   (new ChiselStage).execute(
     args,
     Seq(
-      ChiselGeneratorAnnotation(() => new DepRegEntry)
+      ChiselGeneratorAnnotation(() => new ArithInstQueue)
     )
   )
 }
