5a9e7a3cfeaca276ac3806e2f31b391e0ef700c1
lizhaohan <18811782369@163.com>
2022-04-21 22:08:18 +0800

    Wmb v0.2

--- src/main/scala/Core/LSU/Wmb.scala
+++ src/main/scala/Core/LSU/Wmb.scala
@@ -315,6 +315,14 @@ class Wmb extends Module {
   val wmb_create_ptr = RegInit(VecInit(ptr_init))
   val wmb_read_ptr = RegInit(VecInit(ptr_init))
   val wmb_write_ptr = RegInit(VecInit(ptr_init))
+  val wmb_data_ptr = RegInit(VecInit(ptr_init))
+  val wmb_create_ptr_circular = RegInit(false.B)
+  val wmb_read_ptr_circular = RegInit(false.B)
+  val wmb_write_ptr_circular = RegInit(false.B)
+  val wmb_data_ptr_circular = RegInit(false.B)
+
+  val wmb_write_req_addr = Reg(UInt(LSUConfig.PA_WIDTH.W))
+
 
   //Wire
   val wmb_empty = Wire(Bool())
@@ -347,9 +355,7 @@ class Wmb extends Module {
   val wmb_biu_aw_id    = Wire(UInt(5.W))
   val wmb_biu_write_en = Wire(Bool())
 
-  val wmb_create_ptr_next1 = Wire(Bool())
   val wmb_create_vb_success = Wire(Bool())
-  val wmb_data_ptr = Wire(Vec(LSUConfig.WMB_ENTRY, Bool()))
   val wmb_dcache_inst_write_req_hit_idx = Wire(Bool())
 
   val wmb_ce_create_vld = Wire(Bool())
@@ -366,6 +372,32 @@ class Wmb extends Module {
   val wmb_write_biu_dcache_line = Wire(Bool())
   val wmb_write_dcache_success = Wire(Bool())
   val wmb_write_ptr_shift_imme_grnt = Wire(Bool())
+
+  val wmb_create_vld = Wire(Bool())
+  val wmb_create_ptr_next1 = Wire(Vec(LSUConfig.WMB_ENTRY, Bool()))
+  val wmb_read_ptr_shift_vld = Wire(Bool())
+  val wmb_read_ptr_next1 = Wire(Vec(LSUConfig.WMB_ENTRY, Bool()))
+  val wmb_write_ptr_shift_vld = Wire(Bool())
+  val wmb_write_ptr_next = Wire(Vec(8, Vec(LSUConfig.WMB_ENTRY, Bool())))
+  val wmb_write_ptr_set = Wire(Vec(LSUConfig.WMB_ENTRY, Bool()))
+  val wmb_write_ptr_circular_set = Wire(Bool())
+  val wmb_data_ptr_shift_vld = Wire(Bool())
+  val wmb_data_ptr_next1 = Wire(Vec(LSUConfig.WMB_ENTRY, Bool()))
+
+  val wmb_write_req_next_addr_sub = Wire(Vec(4, Bool()))
+  val wmb_write_req_next_addr_plus = Wire(Vec(4, Bool()))
+
+  val wmb_write_req_ready_to_dcache_line = Wire(Vec(4, Bool()))
+
+  val wmb_vb_create_vld = Wire(Bool())
+  val wmb_read_ptr_met_create = Wire(Bool())
+  val wmb_read_req_hit_idx = Wire(Bool())
+  val wmb_read_req_ctc_inst = Wire(Bool())
+  val wmb_read_req_ctc_end = Wire(Bool())
+  val wmb_write_ptr_met_create = Wire(Bool())
+  val wmb_write_req_hit_idx = Wire(Bool())
+  val wmb_data_ptr_met_create = Wire(Bool())
+
 
   //==========================================================
   //                 Instance of Gated Cell
@@ -459,7 +491,7 @@ class Wmb extends Module {
     wmb_entry(i).io.in.wmb_biu_ar_id       := wmb_biu_ar_id
     wmb_entry(i).io.in.wmb_biu_aw_id       := wmb_biu_aw_id
     wmb_entry(i).io.in.wmb_biu_write_en    := wmb_biu_write_en
-    wmb_entry(i).io.in.wmb_create_ptr_next1               := wmb_create_ptr_next1
+    wmb_entry(i).io.in.wmb_create_ptr_next1               := wmb_create_ptr_next1(i)
     wmb_entry(i).io.in.wmb_create_vb_success              := wmb_create_vb_success
     wmb_entry(i).io.in.wmb_data_ptr                       := wmb_data_ptr(i)
     wmb_entry(i).io.in.wmb_dcache_arb_req_unmask          := wmb_dcache_arb_req_unmask
@@ -482,5 +514,141 @@ class Wmb extends Module {
 
     wmb_entry_out(i) := wmb_entry(i).io.out
   }
+
+
+  //==========================================================
+  //                  Maintain pointer
+  //==========================================================
+  //----------------------registers---------------------------
+  //circular bit is to check whether write/read/data ptr equal to create_ptr
+  //+------------+
+  //| create_ptr |
+  //+------------+
+  when(wmb_create_vld){
+    wmb_create_ptr := wmb_create_ptr_next1
+  }
+
+  when(wmb_create_vld && wmb_create_ptr(LSUConfig.WMB_ENTRY-1)){
+    wmb_create_ptr_circular := !wmb_create_ptr_circular
+  }
+
+  //+----------+
+  //| read_ptr |
+  //+----------+
+  when(wmb_read_ptr_shift_vld){
+    wmb_read_ptr := wmb_read_ptr_next1
+  }
+
+  when(wmb_read_ptr_shift_vld && wmb_read_ptr(LSUConfig.WMB_ENTRY-1)){
+    wmb_read_ptr_circular := !wmb_read_ptr_circular
+  }
+
+  //+-----------+
+  //| write_ptr |
+  //+-----------+
+  when(wmb_write_ptr_shift_vld){
+    wmb_write_ptr := wmb_write_ptr_next(1)
+  }
+
+  when(wmb_write_ptr_shift_vld && wmb_write_ptr(LSUConfig.WMB_ENTRY-1)){
+    wmb_write_ptr_circular := !wmb_write_ptr_circular
+  }
+
+  //+----------+
+  //| data_ptr |
+  //+----------+
+  when(wmb_data_ptr_shift_vld){
+    wmb_data_ptr := wmb_data_ptr_next1
+  }
+
+  when(wmb_data_ptr_shift_vld && wmb_data_ptr(LSUConfig.WMB_ENTRY-1)){
+    wmb_data_ptr_circular := !wmb_data_ptr_circular
+  }
+
+  //------------------write ptr set---------------------------
+  //if snq clear read req, then read ptr must be set write_ptr_set
+
+  //------------------pointer encode--------------------------
+  val wmb_read_ptr_encode = OHToUInt(wmb_read_ptr.asUInt)(2,0)
+
+  val wmb_write_ptr_encode = OHToUInt(wmb_write_ptr.asUInt)(2,0)
+
+  val wmb_write_ptr_next3_encode = OHToUInt(wmb_write_ptr_next(3).asUInt)(2,0)
+
+  //------------------condition wires-------------------------
+  val wmb_write_burst_neg = wmb_write_req_addr(5,4) === 3.U(2.W)
+
+  val wmb_write_req_addr_dcache_begin = wmb_write_req_addr(5,4) === 3.U(2.W) &&
+    wmb_write_req_next_addr_sub(1) && wmb_write_req_next_addr_sub(2) && wmb_write_req_next_addr_sub(3) ||
+    wmb_write_req_addr(5,4) === 0.U(2.W) &&
+    wmb_write_req_next_addr_plus(1) && wmb_write_req_next_addr_plus(2) && wmb_write_req_next_addr_plus(3)
+
+  //------------has read resp in next 4 entry-----------------
+  val write_burst_en = !io.in.fromCp0.lsu_wr_burst_dis
+
+  wmb_write_biu_dcache_line := write_burst_en && wmb_write_req_addr_dcache_begin && wmb_write_req_ready_to_dcache_line.asUInt.andR
+
+  val wmb_read_grnt = io.in.fromBusArb.ar_grnt
+  wmb_read_ptr_unconditional_shift_imme := ParallelORR(wmb_entry_out.map(_.read_ptr_unconditional_shift_imme))
+  wmb_read_ptr_chk_idx_shift_imme := ParallelORR(wmb_entry_out.map(_.read_ptr_chk_idx_shift_imme))
+
+  wmb_create_vb_success := wmb_vb_create_vld &&  io.in.fromVB.create_grnt
+  val wmb_write_grnt = io.in.fromBusArb.aw_grnt || wmb_create_vb_success
+
+  wmb_write_req := ParallelORR(wmb_entry_out.map(_.write_req))
+  wmb_write_ptr_unconditional_shift_imme := ParallelORR(wmb_entry_out.map(_.write_ptr_unconditional_shift_imme))
+  wmb_write_ptr_chk_idx_shift_imme := ParallelORR(wmb_entry_out.map(_.write_ptr_chk_idx_shift_imme))
+  val wmb_data_grnt = io.in.fromBusArb.w_grnt
+
+  wmb_data_req := ParallelORR(wmb_entry_out.map(_.data_req))
+  wmb_data_ptr_after_write_shift_imme := ParallelORR(wmb_entry_out.map(_.data_ptr_after_write_shift_imme))
+  wmb_data_ptr_with_write_shift_imme := ParallelORR(wmb_entry_out.map(_.data_ptr_with_write_shift_imme))
+  //-----------------shift condition wires--------------------
+  wmb_read_ptr_shift_imme_grnt := !wmb_read_ptr_met_create && (wmb_read_ptr_unconditional_shift_imme || wmb_read_ptr_chk_idx_shift_imme && !wmb_read_req_hit_idx)
+
+  wmb_read_ptr_read_req_grnt := wmb_read_grnt && (!wmb_read_req_ctc_inst || wmb_read_req_ctc_end)
+
+  wmb_read_ptr_shift_vld := wmb_read_ptr_read_req_grnt || wmb_read_ptr_shift_imme_grnt
+
+  //all shift imme don't see wmb write imme, because write_imme only effective
+  //to wo st
+  wmb_write_ptr_shift_imme_grnt := !wmb_write_ptr_met_create && (wmb_write_ptr_unconditional_shift_imme || wmb_write_ptr_chk_idx_shift_imme && !wmb_write_req_hit_idx)
+
+  wmb_write_ptr_shift_vld := wmb_write_grnt || wmb_write_ptr_shift_imme_grnt
+
+  //set circular
+  wmb_data_ptr_shift_vld := wmb_data_grnt &&  wmb_data_req ||
+    !wmb_data_ptr_met_create && (wmb_data_ptr_after_write_shift_imme || wmb_data_ptr_with_write_shift_imme && wmb_write_ptr_shift_imme_grnt)
+
+  //------------------other pointer---------------------------
+  wmb_create_ptr_next1 := Cat(wmb_create_ptr.asUInt(LSUConfig.WMB_ENTRY-2,0), wmb_create_ptr.asUInt(LSUConfig.WMB_ENTRY-1)).asTypeOf(Vec(LSUConfig.WMB_ENTRY, Bool()))
+
+  wmb_read_ptr_next1 := Cat(wmb_read_ptr.asUInt(LSUConfig.WMB_ENTRY-2,0), wmb_read_ptr.asUInt(LSUConfig.WMB_ENTRY-1)).asTypeOf(Vec(LSUConfig.WMB_ENTRY, Bool()))
+
+  //mem set must use write_ptr_to_next_3
+  wmb_write_ptr_next(1) := Cat(wmb_write_ptr.asUInt(LSUConfig.WMB_ENTRY-2,0), wmb_write_ptr.asUInt(LSUConfig.WMB_ENTRY-1)).asTypeOf(Vec(LSUConfig.WMB_ENTRY, Bool()))
+  wmb_write_ptr_next(2) := Cat(wmb_write_ptr.asUInt(LSUConfig.WMB_ENTRY-3,0), wmb_write_ptr.asUInt(LSUConfig.WMB_ENTRY-1,LSUConfig.WMB_ENTRY-2)).asTypeOf(Vec(LSUConfig.WMB_ENTRY, Bool()))
+  wmb_write_ptr_next(3) := Cat(wmb_write_ptr.asUInt(LSUConfig.WMB_ENTRY-4,0), wmb_write_ptr.asUInt(LSUConfig.WMB_ENTRY-1,LSUConfig.WMB_ENTRY-3)).asTypeOf(Vec(LSUConfig.WMB_ENTRY, Bool()))
+  wmb_write_ptr_next(4) := Cat(wmb_write_ptr.asUInt(LSUConfig.WMB_ENTRY-5,0), wmb_write_ptr.asUInt(LSUConfig.WMB_ENTRY-1,LSUConfig.WMB_ENTRY-4)).asTypeOf(Vec(LSUConfig.WMB_ENTRY, Bool()))
+  wmb_write_ptr_next(5) := Cat(wmb_write_ptr.asUInt(LSUConfig.WMB_ENTRY-6,0), wmb_write_ptr.asUInt(LSUConfig.WMB_ENTRY-1,LSUConfig.WMB_ENTRY-5)).asTypeOf(Vec(LSUConfig.WMB_ENTRY, Bool()))
+  wmb_write_ptr_next(6) := Cat(wmb_write_ptr.asUInt(LSUConfig.WMB_ENTRY-7,0), wmb_write_ptr.asUInt(LSUConfig.WMB_ENTRY-1,LSUConfig.WMB_ENTRY-6)).asTypeOf(Vec(LSUConfig.WMB_ENTRY, Bool()))
+  wmb_write_ptr_next(7) := Cat(wmb_write_ptr.asUInt(0), wmb_write_ptr.asUInt(LSUConfig.WMB_ENTRY-1,LSUConfig.WMB_ENTRY-7)).asTypeOf(Vec(LSUConfig.WMB_ENTRY, Bool()))
+
+  val wmb_write_ptr_to_next3 = (wmb_write_ptr.asUInt | wmb_write_ptr_next(1).asUInt | wmb_write_ptr_next(2).asUInt | wmb_write_ptr_next(3).asUInt).asTypeOf(Vec(LSUConfig.WMB_ENTRY, Bool()))
+
+  wmb_data_ptr_next1 := Cat(wmb_data_ptr.asUInt(LSUConfig.WMB_ENTRY-2,0), wmb_data_ptr.asUInt(LSUConfig.WMB_ENTRY-1)).asTypeOf(Vec(LSUConfig.WMB_ENTRY, Bool()))
+
+  //-------------judge if meet create signal------------------
+  wmb_read_ptr_met_create := (wmb_read_ptr.asUInt === wmb_create_ptr.asUInt) && (wmb_read_ptr_circular === wmb_create_ptr_circular)
+
+  wmb_write_ptr_met_create := (wmb_write_ptr.asUInt === wmb_create_ptr.asUInt) && (wmb_write_ptr_circular === wmb_create_ptr_circular)
+
+  wmb_data_ptr_met_create := (wmb_data_ptr.asUInt === wmb_create_ptr.asUInt) && (wmb_data_ptr_circular === wmb_create_ptr_circular)
+
+  //==========================================================
+  //          Generate signal for sq pop
+  //==========================================================
+  //----------------can't merge signal------------------------
+
 
 }
