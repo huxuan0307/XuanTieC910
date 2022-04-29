f4b1453a66f80f88534e64254a79ed6fe2188dde
huxuan0307 <532391012@qq.com>
2022-04-30 00:48:08 +0800

    DecodeTable: Using List and Array to fit ListLookup

--- src/main/scala/Core/IDU/DecodeTable/AluDecodeTable.scala
+++ src/main/scala/Core/IDU/DecodeTable/AluDecodeTable.scala
@@ -1,7 +1,8 @@
 package Core.IDU.DecodeTable
 
-import Utils.Bits.BinaryConst.{T, F}
+import Utils.Bits.BinaryConst.{F, T}
 import chisel3._
+import chisel3.util.BitPat
 import ISA.RV64I._
 import Core.IDU.Opcode.AluOpcode._
 import Core.IDU.Opcode.ShiftLogicOpcode._
@@ -9,38 +10,38 @@ import Core.IDU.FuncUnit._
 
 object AluDecodeTable {
 
-  val table = Seq(
-    //        Fu   opcode     rs2
-    //        |    |       rs1|
-    //        |    |    rd |  |
-    lui   -> (ALU, LUI, T, F, F),
-    addi  -> (ALU, ADD, T, T, F),
-    slti  -> (ALU, SLT, T, T, F),
-    sltiu -> (ALU, SLT, T, T, F),
-    xori  -> (ALU, XOR, T, T, F),
-    ori   -> (ALU, OR , T, T, F),
-    andi  -> (ALU, AND, T, T, F),
-    slli  -> (ALU, SLL, T, T, F),
-    srli  -> (ALU, SRL, T, T, F),
-    srai  -> (ALU, SRA, T, T, F),
-    addiw -> (ALU, ADDW,T, T, F),
-    slliw -> (ALU, SLLW,T, T, F),
-    srliw -> (ALU, SRLW,T, T, F),
-    sraiw -> (ALU, SRAW,T, T, F),
-    addw  -> (ALU, ADDW,T, T, T),
-    subw  -> (ALU, SUBW,T, T, T),
-    sllw  -> (ALU, SLLW,T, T, T),
-    srlw  -> (ALU, SRLW,T, T, T),
-    sraw  -> (ALU, SRAW,T, T, T),
-    add   -> (ALU, ADD, T, T, T),
-    sub   -> (ALU, SUB, T, T, T),
-    sll   -> (ALU, SLL, T, T, T),
-    slt   -> (ALU, SLT, T, T, T),
-    sltu  -> (ALU, SLTU,T, T, T),
-    xor   -> (ALU, XOR, T, T, T),
-    srl   -> (ALU, SRL, T, T, T),
-    sra   -> (ALU, SRA, T, T, T),
-    or    -> (ALU, OR,  T, T, T),
-    and   -> (ALU, AND, T, T, T),
+  val table : Array[(BitPat, List[UInt])] = Array(
+    //            Fu   opcode     rs2
+    //            |    |       rs1|
+    //            |    |    rd |  |
+    lui   -> List(ALU, LUI, T, F, F),
+    addi  -> List(ALU, ADD, T, T, F),
+    slti  -> List(ALU, SLT, T, T, F),
+    sltiu -> List(ALU, SLT, T, T, F),
+    xori  -> List(ALU, XOR, T, T, F),
+    ori   -> List(ALU, OR , T, T, F),
+    andi  -> List(ALU, AND, T, T, F),
+    slli  -> List(ALU, SLL, T, T, F),
+    srli  -> List(ALU, SRL, T, T, F),
+    srai  -> List(ALU, SRA, T, T, F),
+    addiw -> List(ALU, ADDW,T, T, F),
+    slliw -> List(ALU, SLLW,T, T, F),
+    srliw -> List(ALU, SRLW,T, T, F),
+    sraiw -> List(ALU, SRAW,T, T, F),
+    addw  -> List(ALU, ADDW,T, T, T),
+    subw  -> List(ALU, SUBW,T, T, T),
+    sllw  -> List(ALU, SLLW,T, T, T),
+    srlw  -> List(ALU, SRLW,T, T, T),
+    sraw  -> List(ALU, SRAW,T, T, T),
+    add   -> List(ALU, ADD, T, T, T),
+    sub   -> List(ALU, SUB, T, T, T),
+    sll   -> List(ALU, SLL, T, T, T),
+    slt   -> List(ALU, SLT, T, T, T),
+    sltu  -> List(ALU, SLTU,T, T, T),
+    xor   -> List(ALU, XOR, T, T, T),
+    srl   -> List(ALU, SRL, T, T, T),
+    sra   -> List(ALU, SRA, T, T, T),
+    or    -> List(ALU, OR,  T, T, T),
+    and   -> List(ALU, AND, T, T, T),
   )
 }
