be0beaa16223eeb61dcaad9c1f4505270bf90820
huxuan0307 <532391012@qq.com>
2022-05-25 15:12:31 +0800

    Opcode: Move ShiftLogicOpcode into AluOpcode

--- src/main/scala/Core/IDU/Opcode/AluOpcode.scala
+++ src/main/scala/Core/IDU/Opcode/AluOpcode.scala
@@ -5,21 +5,35 @@ import chisel3._
 trait AluOpcode extends Opcode {
   // alu
   def ADD   : UInt = "b0000000".U
-  def ADDW  : UInt = "b0000001".U
-  def SUB   : UInt = "b0000010".U
-  def SUBW  : UInt = "b0000011".U
-  def SLT   : UInt = "b0000110".U
-  def SLTU  : UInt = "b0001110".U
+  def SUB   : UInt = "b0000001".U
+  def SLT   : UInt = "b0000011".U
   def LUI   : UInt = "b0001000".U
+  def ADDW  : UInt = "b0100000".U
+  def SUBW  : UInt = "b0100001".U
+  def SLTU  : UInt = "b1000011".U
+  // shift
+  def SLL   : UInt = "b0001000".U
+  def SRL   : UInt = "b0001010".U
+  def SRA   : UInt = "b0001011".U
+  def SLLW  : UInt = "b0101000".U
+  def SRLW  : UInt = "b0101010".U
+  def SRAW  : UInt = "b0101011".U
+  // logic
+  def AND   : UInt = "b0010000".U
+  def OR    : UInt = "b0010001".U
+  def XOR   : UInt = "b0010010".U
   // pseudo
-  def MIN   : UInt = "b0010100".U
-  def MINU  : UInt = "b0011100".U
-  def MINW  : UInt = "b0010101".U
-  def MINUW : UInt = "b0011101".U
-  def MAX   : UInt = "b0010000".U
-  def MAXU  : UInt = "b0011000".U
-  def MAXW  : UInt = "b0010001".U
-  def MAXUW : UInt = "b0011001".U
+  def MIN   : UInt = "b0010000".U
+  def MINU  : UInt = "b1010000".U
+  def MINW  : UInt = "b0110000".U
+  def MINUW : UInt = "b1110000".U
+  def MAX   : UInt = "b0010001".U
+  def MAXU  : UInt = "b1010001".U
+  def MAXW  : UInt = "b0110001".U
+  def MAXUW : UInt = "b1110001".U
+
+  def isWordOp(op: UInt)    : Bool = op(5)
+  def isUnsigned(op: UInt)  : Bool = op(6)
 }
 
 object AluOpcode extends AluOpcode
